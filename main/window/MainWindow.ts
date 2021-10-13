02d2ca1dc07caf762bee51945214a538060222e1
nojsja <yangwei13@outlook.com>
2021-10-13 12:12:59 +0800

    fefactor: window manager

---
+++ main/window/MainWindow.ts
@@ -0,0 +1,153 @@
+import { app, BrowserWindow, Tray, Menu, shell } from "electron";
+import isDev from "electron-is-dev";
+import path from "path";
+import os from "os";
+
+import { IpcMainWindowType, TrayMenu } from '../types/extention';
+import { getBestWindowPosition } from "../helpers";
+
+const platform = os.platform();
+
+export default class IpcMainWindow implements IpcMainWindowType {
+  win: BrowserWindow | null
+  tray: Tray | null
+  icon: string
+  trayIcon: string
+  trayMenu: TrayMenu
+  url: string
+  quitting = false
+  width = 420
+  height = 480
+
+  constructor(args?: {
+    width: number,
+    height: number
+  }) {
+    this.width = args?.width ?? this.width;
+    this.height = args?.height ?? this.height;
+    this.win = null;
+    this.tray = null;
+    this.trayMenu = [
+      {
+        label: "Show UI",
+        click: this.show.bind(this)
+      },
+      {
+        label: "Hide UI",
+        click: this.hide.bind(this)
+      },
+      { type: "separator" },
+      {
+        label: "Quit",
+        click: this.quit.bind(this)
+      }
+    ];
+    this.url = isDev
+    ? "http://localhost:3000"
+    : `file://${path.resolve(app.getAppPath(), "build/index.html")}`;
+    this.icon = path.resolve(__dirname, "../../assets/logo.png");
+    this.trayIcon = path.resolve(
+      __dirname,
+      platform === "darwin"
+        ? "../../assets/icons/16x16.png"
+        : "../../assets/logo.png"
+    );
+  }
+
+  create() {
+    return new Promise((resolve, reject) => {
+      this.win = new BrowserWindow({
+        width: this.width,
+        height: this.height,
+        resizable: false,
+        icon: this.icon,
+        webPreferences: {
+          nodeIntegration: true,
+          contextIsolation: false
+        }
+      });
+
+      if (platform === "darwin") {
+        this.win.hide();
+      }
+
+      this.win.setVisibleOnAllWorkspaces(true);
+
+      this.win.on("minimize", (e: Electron.Event) => {
+        e.preventDefault();
+        this.hide();
+      });
+
+      this.win.on("close", e => {
+        if (!this.quitting) {
+          e.preventDefault();
+          this.hide();
+        }
+      });
+
+      this.win.loadURL(this.url);
+      this.win.removeMenu();
+
+      this.win.webContents.on("new-window", (e, url) => {
+        e.preventDefault();
+        shell.openExternal(url);
+      });
+
+      this.win.on('ready-to-show', () => {
+        resolve(this.win)
+      });
+
+      this.win.on('closed', () => {
+        reject(this.win)
+      });
+
+    });
+  }
+
+  createTray () {
+    return new Promise((resolve, reject) => {
+      this.tray = new Tray(this.trayIcon);
+      const menu: Menu = Menu.buildFromTemplate(this.trayMenu);
+
+      if (platform !== "linux") {
+        this.tray.on("click", e => {
+          if (this.win?.isVisible()) {
+            this.win.hide();
+          } else {
+            this.show();
+          }
+        });
+        this.tray.on("right-click", () => {
+          this.tray?.popUpContextMenu(menu);
+        });
+      } else {
+        this.tray?.setContextMenu(menu);
+      }
+
+      resolve(this.tray);
+    });
+  }
+
+  show() {
+    if (platform === "darwin" || platform === "win32") {
+      if (this.win && this.tray) {
+        const position = getBestWindowPosition(this.win, this.tray);
+        this.win.setPosition(position.x, position.y);
+      }
+    }
+    this.win?.show();
+  }
+
+  hide() {
+    this.win?.hide();
+  }
+
+  beforeQuitting() {
+    this.quitting = true;
+  }
+
+  quit() {
+    this.quitting = true;
+    app.quit();
+  }
+}
---
+++ main/window/TransparentWindow.ts
@@ -0,0 +1,65 @@
+import { app, BrowserWindow, screen } from "electron";
+import path from "path";
+
+import { TransparentWindowType, rectPoint } from '../types/extention';
+
+export default class Transparentwindow implements TransparentWindowType {
+  icon: string
+  url: string
+  width: number
+  height: number
+  win: BrowserWindow | null;
+
+  constructor() {
+    const screenSize = screen.getPrimaryDisplay().workAreaSize;
+    this.icon = path.resolve(__dirname, "../../assets/logo.png");
+    this.url = `file://${path.resolve(app.getAppPath(), "assets/twin.html")}`;
+    this.win = null;
+    this.width = screenSize.width;
+    this.height = screenSize.height;
+  }
+
+  create(params: { fillRect: rectPoint[] }): Promise<any> {
+
+    this.win = new BrowserWindow({
+      width: this.width,
+      height: this.height,
+      transparent: true,
+      alwaysOnTop: false,
+      show: false,
+      fullscreen: true,
+      frame: false,
+      titleBarStyle: 'hidden',
+      resizable: false,
+      icon: this.icon,
+      webPreferences: {
+        webSecurity: false,
+        nodeIntegration: true,
+        contextIsolation: false
+      }
+    });
+
+    this.win.setVisibleOnAllWorkspaces(true);
+    this.win.setFullScreen(true);
+    this.win.removeMenu();
+    this.win.maximize();
+
+    this.win.loadURL(this.url);
+
+    return new Promise(resolve => {
+      this.win?.on('ready-to-show', () => {
+        this.win?.show();
+        resolve(this.win);
+        this.win?.webContents.send('renderer:twin', {
+          action: 'fillRect',
+          params: params.fillRect || []
+        });
+      })
+    });
+  }
+
+  destroy() {
+    this.win?.hide();
+    this.win?.destroy();
+  }
+}
