228e7c37572e7fcb65b74c0bbbac42c2e2e874e1
nojsja <yangwei13@outlook.com>
2023-03-01 12:27:31 +0800

    feat: Add prettier config.

---
+++ .prettierrc.js
@@ -0,0 +1,20 @@
+
+module.exports = {
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  jsxSingleQuote: false,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  arrowParens: 'always',
+  requirePragma: false,
+  insertPragma: false,
+  proseWrap: 'preserve',
+  htmlWhitespaceSensitivity: 'ignore',
+  vueIndentScriptAndStyle: false,
+  endOfLine: 'lf',
+  embeddedLanguageFormatting: 'auto',
+};
